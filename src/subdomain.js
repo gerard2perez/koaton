a276e5e6324b54a75c2b32695766258920726607
Gerardo Pérez Pérez <gerard2perez@outlook.com>
2017-02-07 20:52:35 -0600

    fix: Access controll allow headers will be correctly detected

--- src/subdomain.js
+++ src/subdomain.js
@@ -3,8 +3,9 @@ import { routers } from './router';
 const detectsubdomain = async function detectsubdomain (ctx, next) {
 	let [subdomain = 'www'] = ctx.request.subdomains;
 	ctx.subdomain = subdomain;
-	if (ctx.request.origin.indexOf(configuration.server.host) > -1) {
-		ctx.response.set('Access-Control-Allow-Origin', ctx.request.origin);
+	let origin = ctx.headers.origin ? ctx.headers.origin : ctx.request.origin;
+	if (origin.indexOf(configuration.server.host) > -1) {
+		ctx.response.set('Access-Control-Allow-Origin', origin);
 		ctx.response.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	}
 	await routers(ctx.subdomain).secured.call(ctx, next);
